de9b10582e31385f3cdfdcf2f8de0d9d3dcf0bdc
Gwendal Piemontesi <114938783+gwendalpiemonte@users.noreply.github.com>
2023-10-09 13:16:27 +0200

    feat: add the run time

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -4,10 +4,19 @@ import ch.heigvd.PicoCLI.PicoCLI;
 
 import picocli.CommandLine;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
         CommandLine main = new CommandLine(new PicoCLI());
         main.execute(args);
-        System.out.println("Conversion done!");
+
+        System.out.println("Conversion done !");
+
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd MM yyyy");
+        String formattedTime = currentTime.format(formatter);
+        System.out.println(formattedTime);
     }
 }
