3ea8a2e497ef4dd148adb2fb98db6171eee015fe
Gwendal Piemontesi <114938783+gwendalpiemonte@users.noreply.github.com>
2023-10-09 13:31:43 +0200

    feat: add the "execution" time

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -9,14 +9,22 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         CommandLine main = new CommandLine(new PicoCLI());
         main.execute(args);
 
-        System.out.println("Conversion done !");
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        System.out.println("Exit code      : ");
+
+        System.out.println("Execution time : " + executionTime + " millisecondes");
 
         LocalDateTime currentTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd MM yyyy");
         String formattedTime = currentTime.format(formatter);
-        System.out.println(formattedTime);
+        System.out.println("Actual time    : " + formattedTime);
+
     }
 }
